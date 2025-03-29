8a18023113638da111d63acae4c6f2d8984f71d5
Tanyakalra20 <160746171+Tanyakalra20@users.noreply.github.com>
2025-03-29 13:06:00 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-  // If the token doesn't exist, redirect to login page
+  
   return token ? children : <Navigate to="/" replace />;
 };
 
