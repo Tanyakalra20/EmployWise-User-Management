ffb1dbaad38725f1de4dc7c14347a6d79ae6d154
Tanyakalra20 <160746171+Tanyakalra20@users.noreply.github.com>
2025-03-29 13:09:59 +0530

    Update authService.jsx

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -5,7 +5,7 @@ const BASE_URL = "https://reqres.in/api";
 export const loginUser = async (email, password) => {
     try {
         const response = await axios.post(`${BASE_URL}/login`, { email, password });
-        return response.data; // Should return token
+        return response.data; 
     } catch (error) {
         console.error("Login failed:", error);
         throw error;
